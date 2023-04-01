5848139a786c2a2a3f3485012d1b43dc8b90b893
Arnav Agrawal <arnavagrawal2701@gmail.com>
2023-04-01 12:12:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, request, render_template, redirect
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import io
+from PIL import Image
+from io import BytesIO
 
 model = keras.models.load_model('cifar10.h5')
 
@@ -28,7 +29,9 @@ def predict():
         return jsonify(response)
 
 def preprocess(image_file):
-    image = tf.keras.preprocessing.image.load_img(image_file.read(), target_size=(32,32))
+    image = Image.open(BytesIO(image_file.read()))
+
+    image = image.resize((32, 32))
     
     image = tf.keras.preprocessing.image.img_to_array(image)
 
